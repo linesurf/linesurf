6b7d303dde7d417de08f2be1a2e232f338695d1b
Royce551 <poohwie2005@gmail.com>
2019-12-30 13:54:19 -0600

    i have to commit first

--- Linesurf/LinesurfGame.cs
+++ Linesurf/LinesurfGame.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Linesurf
 {
@@ -12,7 +14,17 @@ namespace Linesurf
         GameTime toShowTime;
         SpriteFont font;
         public static Texture2D Pixel;
-
+        private float x;
+        private Vector2 vector2 = new Vector2(1, 50);
+        private bool timeron = false;
+        private bool showthing = false;
+        private float currentTime = 0;
+        int counter = 1;
+        int limit = 999999;
+        float countDuration = 1f;
+        private SoundEffect effect;
+        bool playsoundeffect = false;
+        Song song;
         public LinesurfGame()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -20,6 +32,8 @@ namespace Linesurf
             IsMouseVisible = true;
             graphics.SynchronizeWithVerticalRetrace = false;
             IsFixedTimeStep = false;
+            TargetElapsedTime = TimeSpan.FromMilliseconds(1000);
+            
         }
 
         protected override void Initialize()
@@ -27,7 +41,7 @@ namespace Linesurf
             // TODO: Add your initialization logic here
             Pixel = new Texture2D(GraphicsDevice, 1, 1);
             Pixel.SetData(new[] { Color.White });
-
+            
             base.Initialize();
         }
 
@@ -35,12 +49,39 @@ namespace Linesurf
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("fontnormal");
+            effect = Content.Load<SoundEffect>("normal-hitnormal");
             
+            this.song = Content.Load<Song>("shutter");
+            MediaPlayer.Volume = 0.17f;
+            MediaPlayer.Play(song);
+            timeron = true;
         }
 
         protected override void Update(GameTime gameTime)
         {
+            if (timeron) if (gameTime != null)
+                {
+                    currentTime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+                    showthing = false;
+                }
+                else showthing = true;
+            if (currentTime >= countDuration)
+            {
+                counter++;
+              
+                currentTime -= countDuration; 
+            }
+            if (counter % 461 == 0) playsoundeffect = true;
+            if (playsoundeffect)
+            {
+                effect.Play(0.20f, 0f, 0f);
+                playsoundeffect = false;
+            }
+            
             gameTime = toShowTime;
+            
+            
+            
             base.Update(gameTime);
         }
 
@@ -48,7 +89,10 @@ namespace Linesurf
         {
             graphics.GraphicsDevice.Clear(Color.DimGray);
             spriteBatch.Begin();
-            spriteBatch.DrawString(font, gameTime.ElapsedGameTime.TotalMilliseconds + "ms", Vector2.One, Color.Aqua);
+            spriteBatch.DrawString(font, "uwu owo, what's this??????? " + gameTime.ElapsedGameTime.TotalMilliseconds + "ms " + counter.ToString(), Vector2.One, Color.Aqua);
+            
+            spriteBatch.DrawString(font, "OWO!!!!!!!!", vector2, Color.Aqua);
+            if (showthing) spriteBatch.DrawString(font, "uwu owo, what's this??????", Vector2.One, Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
         }
