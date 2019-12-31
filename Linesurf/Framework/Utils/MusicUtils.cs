e112e1f7af198ef39e09b41a429d576339074b9a
Vrabbers <nininininimocp@gmail.com>
2019-12-31 19:28:36 -0300

    Update MusicUtils.cs

--- Linesurf/Framework/Utils/MusicUtils.cs
+++ Linesurf/Framework/Utils/MusicUtils.cs
@@ -3,5 +3,7 @@
     public static class MusicUtils
     {
         public static float ToMsOffset(float bpm) => 60_000f / bpm;
+
+        public static float ToBpm(float ms) => 60_000f / ms;
     }
 }
