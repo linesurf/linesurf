c4e50933aa3ff62e0e1a74811e7da5c15440a94e
Royce551 <poohwie2005@gmail.com>
2020-01-04 12:25:40 -0600

    Add Monogame.Extended (and Newtonsoft.Json 'cuz it's dependency)

--- Linesurf/LinesurfGame.cs
+++ Linesurf/LinesurfGame.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-
+using MonoGame.Extended;
 namespace Linesurf
 {
     public class LinesurfGame : Game
